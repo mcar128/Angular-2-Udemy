b2f05c511b9ffce984fa20ffc2a7f66a85ca20a0
mcar <mcar128@gmail.com>
2016-12-13 21:19:54 +0100

    section 5 acomplished, added ngFor to recipes and a cutom dropdown directive, some refactoring of shared classes

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Recipe } from '../recipe';
+import { Recipe } from '../../shared/recipe';
 
 @Component({
   selector: 'rb-recipe-list',
@@ -7,9 +7,13 @@ import { Recipe } from '../recipe';
 })
 export class RecipeListComponent implements OnInit {
 
-	recipes: Recipe[] = [];
-	recipe = new Recipe ('Dummy recipe', 'Lorem Ipsum', 'http://politibits.blogs.tuscaloosanews.com/files/2010/07/sanford_big_dummy_navy_shirt.jpg');
-	@Output() recipeSelected = new EventEmitter<Recipe>();
+	recipes: Recipe[] = [
+    new Recipe ('Wiener schnitzel', 'Very tasty',
+    'http://www.tportal.hr/ResourceManager/GetImage.aspx?imgId=728229&shotId=1&width=630&height=360', []),
+    new Recipe ('Summer sallad', 'Okayish', 'https://www.femina.hr/images/articles/images/2012/salata_jaja_cijela.jpg', [])
+  ];
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
--- recipe-book/src/app/recipes/recipes.component.ts
+++ recipe-book/src/app/recipes/recipes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe';
+import { Recipe } from '../shared/recipe';
 
 @Component({
   selector: 'rb-recipes',
